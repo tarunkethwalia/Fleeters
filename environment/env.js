0f60c5c0e37694e8e2cfb8d4ee2030e22205580a
SillyZan <tarunkethwalia18@gmail.com>
2020-06-12 20:56:12 +0530

    Changed env to json

--- environment/env.js
+++
@@ -1,6 +0,0 @@
-env = {
-
-    MongoUrl:"mongodb+srv://fleeter123:fleeter123@cluster0-l8uki.mongodb.net/beta?retryWrites=true&w=majority"
-
-}
-module.exports =env;
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const cors = require('cors');
 const app = express();
-const bodyParser=require('body-parser');
-const demandRoute= require('./routes/demandRoute');
-const env =require('./environment/env');
+const bodyParser = require('body-parser');
+const demandRoute = require('./routes/demandRoute');
+const env = require('./environment/env').env;
 
-mongoose.connect(process.env.MongoUrl||env.MongoUrl,{
+mongoose.connect(process.env.MongoUrl || env.MongoUrl,{
     useNewUrlParser: true,
     useUnifiedTopology:true
 }).then(()=>{console.log("Mongo Connection Successful.")}).catch(err=>console.error(err));
@@ -21,6 +21,6 @@ app.use('/api',demandRoute);
 
 const port =5000;
 app.listen(port,()=>{
-    console.log(`Server started at post ${port}`);
+    console.log(`Server started at port: ${port}`);
 });
 
