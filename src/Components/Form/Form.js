da9c60863b063af6cd2682d726f73d42f381483c
SillyZan <tarunkethwalia18@gmail.com>
2020-06-14 13:51:26 +0530

    Form Design V1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,11 +5,6 @@ import SideBar from "../SideBar/SideBar";
 import CssUtilities from "../../Utils/Autocomplete";
 
 class Form extends Component {
-    handleChange1 = (e) => {
-        /*An array containing all the country names in the world:*/
-        var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-        CssUtilities.Autocomplete(e.target,countries);
-    }
     render() {
         return (
             <div className="Form">
@@ -26,61 +21,59 @@ class Form extends Component {
                             </div>
                             <div className="consignerInfo">
                                 <div className="input-field">
-                                    <input type="text" id="autocomplete-input" className="autocomplete" />
+                                    <input type="text" id="autocomplete-input" className="autocomplete"/>
                                     <label for="autocomplete-input">Select Lane</label>
-                                    <button class="waves-effect waves-light btn laneButton">Add</button>
+                                    <div className="buttonFlex">
+                                        <button className="waves-effect waves-light btn laneButton">Add</button>
+                                    </div>
                                 </div>
                                 <div className="input-field">
-                                    <input type="text" id="autocomplete-input2" className="autocomplete2" autoComplete="off" onChange={this.handleChange1} />
+                                    <input type="text" id="autocomplete-input2" className="autocomplete2"/>
                                     <label for="autocomplete-input2">Select Consigner</label>
-                                    <button className="waves-effect waves-light btn">Edit</button>
-                                    <button className="waves-effect waves-light btn">Add</button>
+                                    <div className="buttonFlex">
+                                        <button className="waves-effect waves-light btn">Edit</button>
+                                        <button className="waves-effect waves-light btn">Add</button>
+                                    </div>
                                 </div>
                                 <div className="input-field"></div>
                             </div>
                             <div className="consignerAutofill">
-                                <p>Starting Point</p>
-                                <p>Ending Point</p>
-                                <p>Distance(In Km.}</p>
+                                <p className="input-field">Starting Point</p>
+                                <p className="input-field">Ending Point</p>
+                                <p className="input-field">Distance (In Km.}</p>
+                            </div>
+                        </div>
+                        <div className="demandTimings">
+                            <div className="timingHeading">
+                                <h5>Timings</h5>
+                            </div>
+                            <div className="timingsBody">
+                                <div className="input-field">
+                                    <input id="indent-time" type="text" className="validate"/>
+                                    <label htmlFor="indent-time">Indent Time</label>
+                                </div>
+                                <div className="input-field">
+                                    <input id="indent-close-time" type="text" className="validate"/>
+                                    <label htmlFor="indent-close-time">Indent Closing Time</label>
+                                </div>
+                                <div className="input-field">
+                                    <input id="loading-time" type="text" className="validate"/>
+                                    <label htmlFor="loading-time">Loading Time</label>
+                                </div>
+                            </div>
+                            <div className="timingsBody2">
+                                <div className="input-field">
+                                    <input id="TAT" type="text" className="validate"/>
+                                    <label htmlFor="TAT">TAT</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="demandAddress">
+                            <div className="addressHeading">
+                                <h5>Address</h5>
                             </div>
                         </div>
                     </div>
-                    {/*<div className="demandTimings">*/}
-                    {/*    Timings*/}
-                    {/*    <div>*/}
-                    {/*        <TextField*/}
-                    {/*            id="datetime-local"*/}
-                    {/*            label="Indent Time"*/}
-                    {/*            type="datetime-local"*/}
-                    {/*            defaultValue="2017-05-24T10:30"*/}
-                    {/*            className={useStyles.textField}*/}
-                    {/*            InputLabelProps={{*/}
-                    {/*                shrink: true,*/}
-                    {/*            }}/>*/}
-                    {/*    </div>*/}
-                    {/*    <div>*/}
-                    {/*        <TextField*/}
-                    {/*            id="datetime-local"*/}
-                    {/*            label="Closing Time"*/}
-                    {/*            type="datetime-local"*/}
-                    {/*            defaultValue="2017-05-24T10:30"*/}
-                    {/*            className={useStyles.textField}*/}
-                    {/*            InputLabelProps={{*/}
-                    {/*                shrink: true,*/}
-                    {/*            }}/>*/}
-                    {/*    </div>*/}
-                    {/*    <div>*/}
-                    {/*        <TextField*/}
-                    {/*            id="datetime-local"*/}
-                    {/*            label="Loading Time"*/}
-                    {/*            type="datetime-local"*/}
-                    {/*            defaultValue="2017-05-24T10:30"*/}
-                    {/*            className={useStyles.textField}*/}
-                    {/*            InputLabelProps={{*/}
-                    {/*                shrink: true,*/}
-                    {/*            }}/>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                     {/*<div className="demandAddress">*/}
                     {/*    Addresses*/}
                     {/*    <div className="loadingAddress">*/}
