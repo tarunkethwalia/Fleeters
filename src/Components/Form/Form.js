057ba838210c9a4c8c3c94f08e06748d59c8b5bb
SillyZan <tarunkethwalia18@gmail.com>
2020-06-14 19:00:57 +0530

    Form Design V4

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Form.css';
+import {Link} from 'react-router-dom'
 import Navbar from "../Navbar/Navbar";
 import SideBar from "../SideBar/SideBar";
 
@@ -151,6 +152,20 @@ class Form extends Component {
                                                 <span>9871917358</span>
                                             </div>
                                         </div>
+                                        <div className='POCSubmit'>
+                                            <button className="waves-effect waves-light btn">Add</button>
+                                        </div>
+                                        <div className="POCFooter">
+                                            <div className="footerHeading">
+                                                <h6>Loading</h6>
+                                            </div>
+                                            <div className="footerBody">
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="unloadAddressBody">
                                         <div className="input-field">
@@ -189,6 +204,108 @@ class Form extends Component {
                                                 <span>9871917358</span>
                                             </div>
                                         </div>
+                                        <div className='POCSubmit'>
+                                            <button className="waves-effect waves-light btn">Add</button>
+                                        </div>
+                                        <div className="POCFooter">
+                                            <div className="footerHeading">
+                                                <h6>Unloading</h6>
+                                            </div>
+                                            <div className="footerBody">
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                                <div className="footerDiv">
+                                                    <span className='loadAddress'>Some Dummy address with some dummy data</span>
+                                                    <Link to='/'>View</Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="demandVehicle">
+                                <div className="vehicleHeading">
+                                    <h5>Vehicle</h5>
+                                </div>
+                                <div className="vehicleInfo">
+                                    <div className="input-field">
+                                        <input type="text" id="vehicle-type" className="autocomplete"/>
+                                        <label htmlFor="vehicle-type">Vehicle Type</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="feets" className="autocomplete2"/>
+                                        <label htmlFor="feets">Feets</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="tyres" className="autocomplete2"/>
+                                        <label htmlFor="tyres">Tyres</label>
+                                    </div>
+                                </div>
+                                <div className="vehicleInfo">
+                                    <div className="input-field">
+                                        <input type="text" id="vehicle-size" className="autocomplete"/>
+                                        <label htmlFor="vehicle-size">Vehicle Size</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="item-type" className="autocomplete2"/>
+                                        <label htmlFor="item-type">Item Type</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="tonnage" className="autocomplete2"/>
+                                        <label htmlFor="tonnage">Tonnage</label>
+                                    </div>
+                                </div>
+                                <div className="vehicleInfo">
+                                    <div className="input-field">
+                                        <input type="text" id="freight" className="autocomplete"/>
+                                        <label htmlFor="freight">Freight</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="advance" className="autocomplete2"/>
+                                        <label htmlFor="advance">Advance</label>
+                                    </div>
+                                    <div className="input-field">
+                                        <input type="text" id="percentage" className="autocomplete2"/>
+                                        <label htmlFor="percentage">Percentage</label>
+                                    </div>
+                                </div>
+                                <div className="vehicleInfo">
+                                    <div className="HQWrapper">
+                                        <span>HQ</span>
+                                        <div className="switch">
+                                            <label>
+                                                No
+                                                <input type="checkbox" />
+                                                <span className="lever"></span>
+                                                Yes
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="demandCount">
+                                <div className="countHeading">
+                                    <h5>No. of Counts</h5>
+                                </div>
+                                <div className="countBody">
+                                    <div className="input-field">
+                                        <input id="count" type="text" className="validate"/>
+                                        <label htmlFor="count">Count</label>
                                     </div>
                                 </div>
                             </div>
